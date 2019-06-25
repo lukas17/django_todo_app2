e251322769dc4623ac72b08656240caff8edea10
lukas17 <liewlihao@gmail.com>
2019-06-26 01:16:51 +0800

    Update

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('', include('todo.urls')), # Include everything from todo/url.py
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('accounts.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
 ]
