30f8395d2e6eed1035a6dd2641e6e4944933f6cb
lukas17 <liewlihao@gmail.com>
2019-06-25 16:19:39 +0800

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')), # Include everything from todo/url.py
-    path('accounts/', include('django.contrib.auth.url')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
