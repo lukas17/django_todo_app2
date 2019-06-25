833f104da47c4e683a173c9d43ef821660c97cd2
lukas17 <liewlihao@gmail.com>
2019-06-25 17:18:54 +0800

    Update

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')), # Include everything from todo/url.py
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', )
 ]
