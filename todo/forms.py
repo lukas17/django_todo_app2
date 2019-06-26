d1c1c8875a67e46e23cf257326a1f1dfa2ff5380
lukas17 <liewlihao@gmail.com>
2019-06-26 19:14:48 +0800

    Update

--- todo/forms.py
+++ todo/forms.py
@@ -6,6 +6,6 @@ class TodoForm(forms.Form):
             attrs={'class' : 'form-control', 'placeholder' : 'Enter todo e.g. Delete junk files', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
     userEmailInput = forms.CharField(max_length=40, 
         widget=forms.TextInput(
-            attrs={'class' : 'form-control', 'placeholder' : 'Enter todo e.g. Delete junk files', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
+            attrs={'class' : 'form-control', 'placeholder' : 'Enter email', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
 
 
