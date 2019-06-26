16cc751842825e1a11e608e9a1744a00fc3ac977
lukas17 <liewlihao@gmail.com>
2019-06-26 19:07:59 +0800

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -11,9 +11,4 @@ urlpatterns = [
     path('deleteall', views.deleteAll, name='deleteall'),
     path('about/', AboutView.as_view()),
     path('app2/', views.index, name='app2'),
-    path('app3/', views.app3, name='app3'),
-    path('app3/add3', views.addTodo3, name='add3'),
-    path('app3/complete3/<todo_id>', views.completeTodo3, name='complete3'),
-    path('app3/deletecomplete3', views.deleteCompleted3, name='deletecomplete3'),
-    path('app3/deleteall3', views.deleteAll3, name='deleteall3'),
 ]
