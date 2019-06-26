0e883b99da954cfaa0bd70d304e30231f12d0222
lukas17 <liewlihao@gmail.com>
2019-06-26 21:12:53 +0800

    Update

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Todo(models.Model):
     text = models.CharField(max_length=40)
-    writer = models.EmailField()
+    writer = models.EmailField(widget=forms.EmailInput({'placeholder': 'test@example.com'}))
     complete = models.BooleanField(default=False)
 
     def __str__(self):
